59a5f2959b654db9851c62fd1b8d389b873ea36c
Ravan <ravan2003@gmail.com>
2025-07-02 01:55:26 +0800

    Trying integration again

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
 });
